31656e7fcbe967e082a014be07f175329cc3ee0d
madsll <madsll@mp.aau.dk>
2025-10-07 11:15:37 +0200

    No major revision

--- llmtest.py
+++ llmtest.py
@@ -1,6 +1,6 @@
 from langchain_ollama import OllamaLLM
 
-llm = OllamaLLM(model="llama3.1:8b")
-response = llm.invoke("What is the capital of Denmark?")
+llm = OllamaLLM(model="llama3.1:70b")
+response = llm.invoke("What should a search action plan consist of for maritime search and rescue?")
 print(response)
 
